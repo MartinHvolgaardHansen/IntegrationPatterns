405a671206a044089f41230f7c3dd2c9ca18c34a
Martin Hansen <m4h@outlook.dk>
2021-03-23 18:58:24 +0100

    Dotnet restore

--- IntegrationPatterns.Canonical.Translator/TranslatorConsole.cs
+++ IntegrationPatterns.Canonical.Translator/TranslatorConsole.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using JsonPatch;
+using JsonPatch.Formatting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace IntegrationPatterns.Canonical.Translator
 {
-	class Program
+	class TranslatorConsole
 	{
 		static void Main(string[] args)
 		{
